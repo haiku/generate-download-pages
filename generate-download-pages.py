5f071b60b8e17f4841315157f9120c11686bf27b
Niels Sascha Reedijk <niels.reedijk@gmail.com>
2014-07-05 08:42:53 +0200

    Finish the processing and the output of the data pages

--- generate-download-pages.py
+++ generate-download-pages.py
@@ -1,5 +1,7 @@
+from collections import defaultdict, OrderedDict, namedtuple
 import os
-from mako.template import Template
+import re
+
 from mako.lookup import TemplateLookup
 
 #
@@ -8,6 +10,13 @@ from mako.lookup import TemplateLookup
 
 TEMPLATE_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "templates/")
 ARCHIVE_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "FILES", "nightly-images")
+IMAGE_TYPES = (
+    # ("filename_type", "pretty type")
+    ("anyboot", "Anyboot"),
+    ("vmware", "VMDK"),
+    ("raw", "Raw"),
+    ("cd", "ISO"),
+)
 
 #
 # Variants
@@ -22,10 +31,61 @@ VARIANTS = (
 # Process
 #
 
+Image = namedtuple("Image", ['filename', 'revision', 'image_type'])
+Row = type("Row", (object,), {})
+
+def natural_sort_key(s, _nsre=re.compile('([0-9]+)')):
+    return [int(text) if text.isdigit() else text.lower()
+            for text in re.split(_nsre, s)]
+
+
+def headers():
+    return list(q for _,q in IMAGE_TYPES)
+
+
+def index_archives(archive_dir):
+    # reverse sort because we want the newest first
+    # use natural sorting from when we switch from 5 digit hrev to 6 digits
+    files = sorted(os.listdir(archive_dir), key=natural_sort_key, reverse=True)
+
+    images = []
+    for archive in files:
+        m = re.match(r'.*(hrev[0-9]*)-([^-]*)-([^\.]*)\.zip', archive)
+        if m:
+            images.append(Image(archive, m.group(1), m.group(3)))
+
+    # sort the images into a table-like structure that will be used to create the table
+    variant_columns = list(q for q,_ in IMAGE_TYPES)
+    content = OrderedDict()
+
+    for image in images:
+        if image.image_type not in variant_columns:
+            print "Unknown image type for " + image.filename
+            continue
+
+        if image.revision not in content.keys():
+            content[image.revision] = defaultdict(str)
+
+        content[image.revision][image.image_type] = image.filename
+
+    # flatten into a table
+    table = []
+    for revision, links in content.items():
+        row = Row()
+        row.revision = revision
+        row.variants = []
+        for variant in variant_columns:
+            row.variants.append(links[variant])
+        table.append(row)
+
+    return table
+
+
 if __name__ == "__main__":
     template_lookup = TemplateLookup(directories=[TEMPLATE_DIR])
 
     for variant in VARIANTS:
+        table = index_archives(os.path.join(ARCHIVE_DIR, variant[1]))
         template = template_lookup.get_template(variant[0])
         out_f = open(os.path.join(ARCHIVE_DIR, variant[1], "index.html"), "w")
-        out_f.write(template.render())
+        out_f.write(template.render(headers=headers(), table=table))
